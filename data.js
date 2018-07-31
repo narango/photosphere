256673fdc1ab6b33e0d31c7012fc3d2a51dde4a7
Narendra Yenugula <narendra.yng4@gmail.com>
2018-07-31 12:46:00 +0200

    photosphere code added

---
+++ data.js
@@ -0,0 +1,34 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-ktulhu2",
+      "name": "ktulhu2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 500,
+      "initialViewParameters": {
+        "yaw": 0.16642049363717426,
+        "pitch": 0.1739943739606158,
+        "fov": 1.3185972797531151
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
